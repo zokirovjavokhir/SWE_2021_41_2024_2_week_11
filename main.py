523b4f91a54de38381b16a45f5dc9be1740a5ded
zokirovjavokhir <javzokirov04@g.skku.edu>
2024-11-13 20:39:50 +0900

    fixed things again

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import List
+
+def path_to_file_list(path: str) -> List[str]:
+    """Reads a file and returns a list of lines in the file"""
+    # Will this be working?
+    lines = open(path, 'r').read().split('\n')
+    return lines
+
+def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
+    """Converts two lists of file paths into a list of json strings"""
+    pass
+
+def write_file_list(file_list: List[str], path: str) -> None:
+    """Writes a list of strings to a file, each string on a new line"""
+    with open(path, 'w') as f:
+        for file in file_list:
+            f.write(file + '\n')
+            
+if __name__ == "__main__":
+    path = './'
+    german_path = './german.txt'
+    english_path = './english.txt'
+
+    english_file_list = path_to_file_list(english_path)
+    german_file_list = path_to_file_list(german_path)
+
+    processed_file_list = train_file_list_to_json(english_file_list, german_file_list)
+
+    write_file_list(processed_file_list, path+'concated.json')
